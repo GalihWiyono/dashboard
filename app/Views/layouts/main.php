dee6b540abcb8d47931f479840dea6cc523bfd8a
Galih <galihputra2201@gmail.com>
2025-08-05 16:51:42 +0700

    fix: asset link

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -42,12 +42,12 @@
 
     </div>
   </div>
-  <script src="<?= base_url('assets/libs/jquery/dist/jquery.min.js') ?>"></script>
-  <script src="<?= base_url('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
+  <script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+  <script src="<?= base_url('assets/js/bootstrap.bundle.min.js') ?>"></script>
   <script src="<?= base_url('assets/js/sidebarmenu.js') ?>"></script>
   <script src="<?= base_url('assets/js/app.min.js') ?>"></script>
-  <script src="<?= base_url('assets/libs/apexcharts/dist/apexcharts.min.js') ?>"></script>
-  <script src="<?= base_url('assets/libs/simplebar/dist/simplebar.js') ?>"></script>
+  <script src="<?= base_url('assets/js/apexcharts.min.js') ?>"></script>
+  <script src="<?= base_url('assets/js/simplebar.js') ?>"></script>
   <script src="<?= base_url('assets/js/dashboard.js') ?>"></script>
   <script src="<?= base_url('assets/js/datatables.js') ?>"></script>
   <script src="<?= base_url('assets/js/dashboards.src.js') ?>"></script>
